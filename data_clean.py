ace68c32a14bfa48bd1859fa1cc08ca5473acb71
Jack O'Connor <j.oconnor@yale.edu>
2023-03-03 19:49:34 -0500

    database fully prepared for analysis

--- data_clean.py
+++ data_clean.py
@@ -1,4 +1,5 @@
 import logging
+# import json
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -6,7 +7,7 @@ from sqlalchemy.exc import IntegrityError
 
 import statsapi
 
-from models import Player, Pitcher, Batter, Base
+from models import Team, Game, Play, Player, Pitcher, Batter, Base
 
 
 DB_FILE = 'data/mlb.db'
@@ -45,15 +46,50 @@ def read_all_players(seasons):
                 session.rollback()
                 logger.warning('Player {} already exists'.format(p['id']))
 
-def read_all_games(seasons):
-    pass
-            
+
+def read_all_teams():
+    teams = statsapi.get('teams', {'sportId': 1})['teams']   
+    for t in teams:
+        new_team = Team(id=t['id'], name=t['name'], abbreviation=t['abbreviation'], locationName=t['locationName'])
+        session.add(new_team)
+        try:
+            session.commit()
+        except IntegrityError:
+            session.rollback()
+            logger.warning('Team {} already exists'.format(t['id']))
+
+
+def read_all_games():
+    print('Retrieving schedule...')
+    games = statsapi.schedule(start_date='04/07/2022', end_date='10/02/2022', sportId=1)
+    print('Schedule received.')
+    for i, g in enumerate(games):
+        if i % 100 == 0:
+            print('Processing game {} of {}'.format(i, len(games)))
+        new_game = Game(id=g['game_id'], date=g['game_date'], homeTeamId=g['home_id'], awayTeamId=g['away_id'], venue=g['venue_name'])
+        session.add(new_game)
+        try:
+            session.commit()
+        except IntegrityError:
+            session.rollback()
+            logger.warning('Game {} already exists'.format(g['game_id']))
+        # get play by play for the game
+        plays = statsapi.get('game_playByPlay', {'gamePk': g['game_id']})['allPlays']
+        for p in plays:
+            new_play = Play(gameId=g['game_id'], result=p['result']['event'], pitcherId=p['matchup']['pitcher']['id'], batterId=p['matchup']['batter']['id'], game=new_game)
+            session.add(new_play)
+            try:
+                session.commit()
+            except IntegrityError:
+                session.rollback()
+                logger.warning('Play already exists for game {}'.format(g['game_id']))
+
 
 def main():
-    # set up sqlalchemy
     Base.metadata.create_all(engine)
-    # read all players
+    read_all_teams()
     read_all_players([2022])
+    read_all_games()
 
 if __name__ == "__main__":
     main()
--- models.py
+++ models.py
@@ -4,6 +4,15 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base= declarative_base()
 
+
+class Team(Base):
+    __tablename__ = 'teams'
+
+    id = Column(Integer, primary_key=True, autoincrement=False)
+    name = Column(String(255), nullable=False)
+    abbreviation = Column(String(10), nullable=False)
+    locationName = Column(String(255), nullable=False)
+
 class Player(Base):
     __tablename__ = 'players'
 
@@ -39,4 +48,25 @@ class Game(Base):
     __tablename__ = 'games'
 
     id = Column(Integer, primary_key=True, autoincrement=False)
+
+    date = Column(String(20))
+    homeTeamId = Column(Integer, ForeignKey('teams.id'))
+    awayTeamId = Column(Integer, ForeignKey('teams.id'))
+    venue = Column(String(255))
+
+    plays = relationship("Play", back_populates="game")
+
+
+class Play(Base):
+    __tablename__ = 'plays'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    gameId = Column(Integer, ForeignKey('games.id'))
+    result = Column(String(255))
+    pitcherId = Column(Integer, ForeignKey('pitchers.id'))
+    batterId = Column(Integer, ForeignKey('batters.id'))
+
+    game = relationship("Game", back_populates="plays")
+
 
